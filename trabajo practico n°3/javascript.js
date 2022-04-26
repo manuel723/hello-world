e28a299f0987fcfee49cce6c2b841f6d5c7aa33f
manuel723 <54875898+manuel723@users.noreply.github.com>
2022-04-26 17:11:28 -0300

    Add files via upload

---
+++ trabajo practico n°3/javascript.js
@@ -0,0 +1,11 @@
+function myFunction(){
+      document.getElementById("demo").innerHTML = "Hola mundo";
+
+      document.getElementById("demo2").innerHTML = "Hola JavaScript ";
+
+      document.getElementById("demo3").innerHTML = "JavaScript!";
+
+      document.getElementById("demo4").innerHTML = "Hola";
+
+      document.getElementById("demo5").innerHTML = "Mundo";
+    }
